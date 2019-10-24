dad0419b7074cf5aadc239e7c1c22f3e61e39f3b
Lekkala Vamsi Krishna <32891857+vamsikrishna2421@users.noreply.github.com>
2019-10-24 12:30:14 +0530

    Update p003.py

--- python/p003.py
+++ python/p003.py
@@ -34,3 +34,31 @@ def smallest_prime_factor(n):
 
 if __name__ == "__main__":
 	print(compute())
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	#better solution
+	
+n = 600851475143
+i = 2
+while i * i < n:
+    while n % i == 0:
+        n = n / i
+    i = i + 1
+
+print n
